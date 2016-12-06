d7202720878ce530cb159d0baf95cda24b833400
unknown <>
2016-12-06 12:28:21 +0900

    チャット機能の実装-修正

--- js/socketio.js
+++ js/socketio.js
@@ -1,17 +1,37 @@
 $(function(){
-    var socket = io.connect();
-    socket.on("server_to_client", function(data){appendMsg(data.value)});
-    function appendMsg(text) {
-        $("#chatLogs").append("<div>" + text + "</div>");
-    }
-    $("#msg-form").submit(function(e){
-        var message = $("#msg").val();
-        $("#msg").val('');
-        socket.emit("client_to_server", {value : message});
-        //window.location.href = "?id=2&" + "name=" $('#lg_username').val() + "&" + "room=" + $('#room-select').val();
-        e.preventDefault();
-        //return false;
-    });
+    var url   = location.href;
+    var params    = url.split("?");
+    var spparams   = params[1].split("&");
+    var paramArray = [];
+    var socket = io.connect("http://localhost:80");
+    // socket.on("server_to_client", function(data){appendMsg(data.value)});
+    // function appendMsg(text) {
+    //     $("#chatLogs").append("<div>" + text + "</div>");
+    // }
+    // $("#msg-form").submit(function(e){
+    //     var message = $("#msg").val();
+    //     $("#msg").val('');
+    //     socket.emit("client_to_server", {value : message});
+    //     //window.location.href = "?id=2&" + "name=" $('#lg_username').val() + "&" + "room=" + $('#room-select').val();
+    //     e.preventDefault();
+    //     //return false;
+    // });
+
+            //emit イベントを発信している
+            //on  イベントを待ち受けている
+            $('#msg-form').submit(function(e){
+                e.preventDefault();
+                socket.json.emit('emit_from_client',{
+                    msg : $('#msg').val(),
+                    name : paramArray["lg_username"],
+                    room : paramArray["rooms"]
+
+                });
+                $('#msg').val('').focus();
+            });
+            socket.on('emit_from_server',function(data){
+                $('#chatLogs').append("<div>" + data + "</div>");
+            });
 });
 
 
