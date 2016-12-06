47dd90e0f20381a89f0742b2ec04507ec5351dc6
unknown <>
2016-12-06 12:05:16 +0900

    チャット機能の実装-修正

--- js/socketio.js
+++ js/socketio.js
@@ -1,20 +1,16 @@
 $(function(){
-
-        var socket = io.connect();
-
+    var socket = io.connect();
     socket.on("server_to_client", function(data){appendMsg(data.value)});
-
     function appendMsg(text) {
         $("#chatLogs").append("<div>" + text + "</div>");
     }
-
     $("#msg-form").submit(function(e){
-        var message = $("#msgForm").val();
-        $("#msgForm").val('');
+        var message = $("#msg").val();
+        $("#msg").val('');
         socket.emit("client_to_server", {value : message});
         //window.location.href = "?id=2&" + "name=" $('#lg_username').val() + "&" + "room=" + $('#room-select').val();
         e.preventDefault();
-        return false;
+        //return false;
     });
 });
 
