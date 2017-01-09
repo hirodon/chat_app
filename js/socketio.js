6912ce1af3bac2767d08da71d06069b7436a8427
佐藤浩仁 <hirodon@hirodonMacBook-Pro.local>
2017-01-09 18:01:57 +0900

    command function created

--- js/socketio.js
+++ js/socketio.js
@@ -68,6 +68,34 @@ $(function(){
         $('#chatLogs').append(data);
         bottom_scroll();
     });
+    function command(comment){
+        var command = "";
+        var value = "";
+        if (comment[0] == "#") {
+            for (var i = 0; comment.length < i; i++) {
+                if (comment[i] != ":" || comment.length != i) {
+                    command += comment[i];
+                }else{
+                    break;
+                }
+            }
+            for(var i = command.length; comment.length < i;i++ ){
+                value += comment[i];
+            }
+            console.log(command +':'+value);
+            switch(command){
+                case "color":
+                    $(".yourself-comment").css("color",value);
+                break;
+                case "background":
+                    $("body").css("background",value);
+                break;
+                default:
+                    chat_alert("読み込みに失敗しました。");
+                break;
+            }
+        }
+    } 
 });
 
 
